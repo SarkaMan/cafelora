5e3470db91317899ada760c2ae3b36de1b3e3eb6
Šárka Manďáková <sarka.mandakova@gmail.com>
2024-04-27 10:47:55 +0200

    5 Ingredience jako komponenty

--- src/components/Drink/index.jsx
+++ src/components/Drink/index.jsx
@@ -1,4 +1,5 @@
 import './style.css';
+import { Layer } from '../Layer';
 
 export const Drink = ({ id, ordered, layers, name, image }) => {
   return (
@@ -9,13 +10,7 @@ export const Drink = ({ id, ordered, layers, name, image }) => {
         </div>
         <div className="drink__info">
           <h3>{name}</h3>
-          <div className="layer">
-            <div
-              className="layer__color"
-              style={{ backgroundColor: '#613916' }}
-            />
-            <div className="layer__label">espresso</div>
-          </div>
+          <Layer color="#feeeca" label="mléčná pěna" />
         </div>
       </div>
       <form className="drink__controls">
